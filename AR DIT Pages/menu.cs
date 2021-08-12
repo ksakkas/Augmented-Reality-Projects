ec60258d7e403da669b67032fe19ab09acb141cc
Konstantinos Sakkas <63365463+ksakkas@users.noreply.github.com>
2021-08-12 11:50:08 +0300

    Add files via upload

---
+++ AR DIT Pages/menu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Διαχείριση Μενού 
+
+public class menu : MonoBehaviour
+{
+    public void scene_students()
+    {
+        SceneManager.LoadScene("STUDENTS");
+    }
+
+    public void scene_professors()
+    {
+        SceneManager.LoadScene("PROFESSORS");
+    }
+
+    public void scene_secr()
+    {
+        SceneManager.LoadScene("SECRETARIAT");
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+        Debug.Log("Exit ok!!!");
+    }
+
+    public void prev()
+    {
+        SceneManager.LoadScene("START");
+    }
+
+    public void t1()
+    {
+        Application.OpenURL("http://myweb.dit.uoi.gr/ksakkas/dit.uoi.gr.k/ar_students.php");
+    }
+
+    public void t2()
+    {
+        Application.OpenURL("http://myweb.dit.uoi.gr/ksakkas/dit.uoi.gr.k/ar_professors.php");
+    }
+
+    public void t3()
+    {
+        Application.OpenURL("http://myweb.dit.uoi.gr/ksakkas/dit.uoi.gr.k/ar_secretariat.php");
+    }
+}
---
+++ AR DIT Pages/url.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class url : MonoBehaviour
+{
+    public void box_1()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/news.php");
+    }
+
+    public void box_2()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/e-class/");
+    }
+
+    public void box_3()
+    {
+        Application.OpenURL("https://classweb.uoi.gr/");
+    }
+
+    public void box_4()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/useful.php?id=6");
+    }
+
+    public void box_5()
+    {
+        Application.OpenURL("https://eudoxus.gr/");
+    }
+
+    public void box_6()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/education.php?id=7");
+    }
+
+    public void box_7()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/members.php");
+    }
+
+    public void box_8()
+    {
+        Application.OpenURL("https://dasta.teiep.gr/");
+    }
+
+    public void box_9()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/secretariat.php");
+    }
+
+    public void box_10()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/newsinsert.php");
+    }
+
+    public void box_11()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/profileedit.php");
+    }
+
+    public void box_12()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/useful.php?id=1");
+    }
+
+    public void box_13()
+    {
+        Application.OpenURL("https://www.uoi.gr/ekpaideysi/entypa-grammateion/");
+    }
+
+    public void box_14()
+    {
+        Application.OpenURL("http://academicid.minedu.gov.gr/");
+    }
+
+    public void box_15()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/files/A8.pdf");
+    }
+}
