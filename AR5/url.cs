e0b995d6da3666eb5edb5128c0b3abca5032cbb5
Konstantinos Sakkas <63365463+ksakkas@users.noreply.github.com>
2021-08-26 13:32:46 +0300

    Add files via upload

---
+++ AR5/url.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class url : MonoBehaviour
+{
+    public void url1()
+    {
+        Application.OpenURL("https://www.dit.uoi.gr/news.php");
+    }
+}
